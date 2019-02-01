6a40dba33b1b965d24277478d978a167885bf5dd
Blake Doty <blaked609@gmail.com>
2019-02-01 16:52:51 -0700

    Added new commands, added RobotMap and updated OI, DriveTrain subsystem

---
+++ src/main/java/frc/robots/RobotMap.java
@@ -0,0 +1,22 @@
+package frc.robots;
+
+import edu.wpi.first.wpilibj.SPI;
+
+public class RobotMap {
+    public static final int 
+        XBOX_PORT = 0,
+        
+        DRIVE_STICK_Y_AXIS = 1,
+        DRIVE_STICK_X_AXIS = 0,
+        
+        LEFT_DRIVE_TALON_CAN_ID = 0,
+        LEFT_DRIVE_VICTOR_CAN_ID = 1,
+
+        RIGHT_DRIVE_TALON_CAN_ID = 2,
+        RIGHT_DRIVE_VICTOR_CAN_ID = 3,
+
+        LIFT_ARM_TALON_CAN_ID = 5;
+        
+
+
+}
